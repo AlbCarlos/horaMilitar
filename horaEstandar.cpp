f7ccf9551f0b0bf1ece85d1a3377d6b18484094c
albnacho <albcarlosrndn@gmail.com>
2021-01-22 15:51:22 -0600

    Se crea la clase horaEstandar.
    Signed-off-by: albnacho <albcarlosrndn@gmail.com>

---
+++ horaEstandar.cpp
@@ -0,0 +1,41 @@
+#include "horaEstandar.h"
+
+horaEstandar::horaEstandar(int h, int m, string ampm) {
+    this-> horas = h;
+    this-> minutos = m;
+    this-> ampm = ampm;
+}
+
+horaEstandar::~horaEstandar() {}
+
+int horaEstandar::getHoras() {
+    return horas;
+}
+
+int horaEstandar::getMinutos() {
+    return minutos;
+}
+
+string horaEstandar::getAmpm() {
+    return ampm;
+}
+
+void horaEstandar::setHoras(int h) {
+    this-> horas = h;
+}
+
+void horaEstandar::setMinutos(int m) {
+    this-> minutos = m;
+}
+
+void horaEstandar::setAmpm(string ampm) {
+    this-> ampm = ampm;
+}
+
+string horaEstandar::toString() {
+    stringstream ss;
+
+    ss << getHoras() << ":" << getMinutos() << " " + getAmpm();
+    
+    return ss.str();
+}
---
+++ horaEstandar.h
@@ -0,0 +1,30 @@
+#ifndef HORAMILITAR_HORAESTANDAR_H
+#define HORAMILITAR_HORAESTANDAR_H
+
+#include <string>
+#include <sstream>
+using namespace std;
+
+class horaEstandar
+{
+private:
+    int horas;
+    int minutos;
+    string ampm;
+public:
+    horaEstandar(int = 0, int = 0, string = "");
+    ~horaEstandar();
+//Getters
+    int getHoras();
+    int getMinutos();
+    string getAmpm();
+
+//Setters
+    void setHoras(int);
+    void setMinutos(int);
+    void setAmpm(string);
+
+    string toString();
+};
+
+#endif //HORA MILITAR HORA ESTANDAR
